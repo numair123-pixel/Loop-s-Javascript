35dda4b76d2311c8dcab774783fd4b89dd8ccadc
Numair <undisguisedtoast@gmail.com>
2022-01-28 21:27:58 +0530

    First commit

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-echo "hello world";
+//Object Constructor Functions
+function Home(rooms, apartment, color, swimingPool, state, city) {
+    this.rooms = rooms;
+    this.apartment = apartment;
+    this.color = color;
+    this.swimmingPool = swimmingPool
+    this.state = state;
+    this.city = city;
+ }
+ var home1 = new Home(3, false, 'blue' true,'J&K' "Srinagar" )
+ var home2 = new Home(5, false, 'red'false,'Delhi' "Noida")
